4052f761dc49b867c2988dad39184e673b84a464
Amio <amio.cn@gmail.com>
2016-01-25 18:29:29 +0800

    Use mustache template to generate html.

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
+var fs = require('fs')
 var fse = require('fs-extra')
 var needle = require('needle')
 var marked = require('marked')
+var mustache = require('mustache')
 
 var uri = 'https://api.github.com/repos/nodejs/node/issues'
 var dist = 'dist/'
 
-function clean () {
-  fse.emptyDir('dist')
-}
+// Clean dist folder
+fse.emptyDirSync('dist')
 
-clean()
+var postTemplate = fs.readFileSync('./templates/post.mustache', 'utf-8')
 
 needle.get(uri, function (err, resp) {
   if (!err && resp.statusCode === 200) {
     var issues = resp.body
     issues.map(issue => {
+      issue.content = marked(issue.body)
+
       var name = dist + issue.created_at + '.html'
-      var content = marked(issue.body)
+      var content = mustache.render(postTemplate, issue)
       fse.outputFile(name, content)
     })
   }
